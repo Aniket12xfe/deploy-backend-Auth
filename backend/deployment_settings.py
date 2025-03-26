f58c52c8d2e28c6b52d8a7e02441e950e5c91279
Aniket Chaudhari <chaudharianiket379@gmail.com>
2025-03-26 23:53:45 +0530

    commit

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -4,7 +4,9 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOST")]
-CSRF_TRUSTED_ORIGINS = ["https://"+os.environ.get("RENDER_EXTERNAL_HOST")]
+
+CSRF_TRUSTED_ORIGINS = ["https://" + os.environ.get("RENDER_EXTERNAL_HOST", "example.com")]
+
 
 DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
