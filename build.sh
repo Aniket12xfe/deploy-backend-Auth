371bbaa43e5b4cac2423485cfc1c28397121ae52
Aniket Chaudhari <chaudharianiket379@gmail.com>
2025-03-27 11:09:38 +0530

    superuser build files changes

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --no-input \
-        --username "$DJANGO_SUPERUSER_USERNAME" \
-        --email "$DJANGO_SUPERUSER_EMAIL"
+    python manage.py createsuperuser --no-input
 fi
 
